b1061c28515101511f593cf8ade8e7690ed9bc00
Kevin Kipp <kevin.kipp@gmail.com>
2017-05-13 22:13:37 -0500

    Guard against empty value

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -4,7 +4,7 @@ var CountdownForm = React.createClass({
   onSubmit: function(e) {
     e.preventDefault();
     var strSeconds = this.refs.seconds.value;
-    if (!isNaN(strSeconds)) {
+    if (!isNaN(strSeconds) && strSeconds !== '') {
       this.refs.seconds.value = '';
       this.props.onSetCountdown(parseInt(strSeconds, 10));
     }
